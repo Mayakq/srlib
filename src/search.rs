c2d57ec1f0bb45901e6dc53a24b5205d3a53a276
mayak <mayak@no.mail>
2024-12-28 22:47:48 +0400

    create tests, search by name

--- src/search.rs
+++ src/search.rs
@@ -1,3 +1,95 @@
+use regex::Regex;
+use walkdir::WalkDir;
+#[derive(Debug)]
+pub enum Searching {
+    Absolute,
+    Relative,
+}
+#[derive(Debug)]
+pub enum Pallet {
+    Regular(Regex),
+    Name(String),
+}
+#[derive(Debug)]
 pub struct Search {
-    
+    path: String,
+    pallet: Pallet,
+    searching: Searching,
+}
+#[derive(Debug)]
+pub enum Error {
+    LenNameZero,
+    LenRegularZero,
+}
+impl Search {
+    pub fn new(path: String, pallet: Pallet, searching: Searching) -> Self {
+        Self {
+            path,
+            pallet,
+            searching,
+        }
+    }
+    pub fn start(&self) -> Result<Vec<String>, Error> {
+        let mut vec = Vec::new();
+        match &self.pallet {
+            Pallet::Regular(regex) => {
+                if regex.captures_len() == 0 {
+                    return Err(Error::LenRegularZero);
+                }
+            }
+            Pallet::Name(name) => {
+                if name.len() == 0 {
+                    return Err(Error::LenNameZero);
+                }
+            }
+        }
+        match self.searching {
+            Searching::Absolute => {
+                let result = self.absolute(&mut vec);
+                if result.is_none() {
+                    return Ok(vec);
+                }
+                return Err(result.unwrap());
+            }
+            Searching::Relative => {
+                let result = self.relative(&mut vec);
+                if result.is_none() {
+                    return Ok(vec);
+                }
+                return Err(result.unwrap());
+            }
+        }
+    }
+    fn absolute(&self, vec: &mut Vec<String>) -> Option<Error> {
+        match &self.pallet {
+            Pallet::Regular(regex) => {}
+            Pallet::Name(name) => {
+                for entry in WalkDir::new(&self.path).same_file_system(true) {
+                    let entry = entry.unwrap();
+                    let path = entry.path().to_str().unwrap().to_string();
+                    let f_name = entry.file_name().to_str().unwrap().to_string();
+                    if *name == f_name {
+                        vec.push(path);
+                    }
+                }
+            }
+        }
+        None
+    }
+    fn relative(&self, vec: &mut Vec<String>) -> Option<Error> {
+        match &self.pallet {
+            Pallet::Regular(regex) => {}
+            Pallet::Name(name) => {
+                for entry in WalkDir::new(&self.path).same_file_system(true) {
+                    let entry = entry.unwrap();
+                    let path = entry.path().to_str().unwrap().to_string();
+                    let f_name = entry.file_name().to_str().unwrap().to_string();
+                    if f_name.contains(name) {
+                        vec.push(path);
+                    }
+                }
+            }
+        }
+        None
+    }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,25 @@
 #[cfg(test)]
-mod tests{
+mod tests {
+    use crate::search::Search;
+
     #[test]
-    fn test_init(){
-        println!("YES")
+    fn test_absolute_name() {
+        let result = Search::new(
+            "./".to_string(),
+            crate::search::Pallet::Name(".gitignore".to_string()),
+            crate::search::Searching::Absolute,
+        );
+        let res = result.start();
+        assert_eq!(*res.unwrap().get(0).unwrap(), "./.gitignore".to_string());
+    }
+    #[test]
+    fn test_relative_name() {
+        let result = Search::new(
+            "./".to_string(),
+            crate::search::Pallet::Name(".gitignor".to_string()),
+            crate::search::Searching::Relative,
+        );
+        let res = result.start();
+        assert_eq!(*res.unwrap().get(0).unwrap(), "./.gitignore".to_string());
     }
 }
