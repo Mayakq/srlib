7e25db663530c7200d0cd7d8a78b57ef3033fefc
mayak <mayak@no.mail>
2025-01-08 15:23:29 +0400

    fix tests

--- src/tests.rs
+++ src/tests.rs
@@ -6,40 +6,44 @@ mod tests {
 
     #[test]
     fn absolute_name() {
+        let binding = crate::search::Pallet::Name(".gitignore".to_string());
         let result = Search::new(
             "./".to_string(),
-            crate::search::Pallet::Name(".gitignore".to_string()),
-            crate::search::Searching::Absolute,
+            &binding,
+            &crate::search::Searching::Absolute,
         );
         let res = result.start();
         assert_eq!(*res.unwrap().get(0).unwrap(), "./.gitignore".to_string());
     }
     #[test]
     fn relative_name() {
+        let binding = crate::search::Pallet::Name(".gitignor".to_string());
         let result = Search::new(
             "./".to_string(),
-            crate::search::Pallet::Name(".gitignor".to_string()),
-            crate::search::Searching::Relative,
+            &binding,
+            &crate::search::Searching::Relative,
         );
         let res = result.start();
         assert_eq!(*res.unwrap().get(0).unwrap(), "./.gitignore".to_string());
     }
     #[test]
     fn regex1() {
+        let binding = crate::search::Pallet::Regular(Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap());
         let result = Search::new(
             "./tests/".to_string(),
-            crate::search::Pallet::Regular(Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap()),
-            crate::search::Searching::Absolute,
+            &binding,
+            &crate::search::Searching::Absolute,
         );
         let res = result.start();
         assert_eq!(*res.unwrap().get(0).unwrap(), "./tests/2010-03-14".to_string());
     }
     #[test]
     fn regex2() {
+        let binding = crate::search::Pallet::Regular(Regex::new(r"^\d{4}-\d{2}-\d{2}$*\.txt").unwrap());
         let result = Search::new(
             "./tests/".to_string(),
-            crate::search::Pallet::Regular(Regex::new(r"^\d{4}-\d{2}-\d{2}$*\.txt").unwrap()),
-            crate::search::Searching::Absolute,
+            &binding,
+            &crate::search::Searching::Absolute,
         );
         let res = result.start();
         assert_eq!(*res.unwrap().get(0).unwrap(), "./tests/2010-03-14.txt".to_string());
