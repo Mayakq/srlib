8a420fb96c7d4471350ac7b400c446b98b67bc93
mayak <mayak@no.mail>
2025-01-08 15:42:51 +0400

    refactoring, add new error

--- src/search.rs
+++ src/search.rs
@@ -1,7 +1,8 @@
 use regex::Regex;
 use walkdir::WalkDir;
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub enum Searching {
+    #[default]
     Absolute,
     Relative,
 }
@@ -10,9 +11,14 @@ pub enum Pallet {
     Regular(Regex),
     Name(String),
 }
+impl Default for Pallet {
+    fn default() -> Self {
+        return Pallet::Name(String::with_capacity(16));
+    }
+}
 #[derive(Debug)]
 pub struct Search<'a> {
-    path: String,
+    path: &'a String,
     pallet: &'a Pallet,
     searching: &'a Searching,
 }
@@ -20,9 +26,10 @@ pub struct Search<'a> {
 pub enum Error {
     LenNameZero,
     LenRegularZero,
+    LenPathZero,
 }
 impl<'a> Search<'_> {
-    pub fn new(path: String, pallet: &'a Pallet, searching: &'a Searching) -> Search<'a> {
+    pub fn new(path: &'a String, pallet: &'a Pallet, searching: &'a Searching) -> Search<'a> {
         Search {
             path,
             pallet,
@@ -30,7 +37,7 @@ impl<'a> Search<'_> {
         }
     }
     pub fn start(&self) -> Result<Vec<String>, Error> {
-        let mut vec = Vec::new();
+        let mut vec = Vec::with_capacity(16);
         match &self.pallet {
             Pallet::Regular(regex) => {
                 if regex.captures_len() == 0 {
@@ -44,14 +51,14 @@ impl<'a> Search<'_> {
             }
         }
         match &self.pallet {
-            Pallet::Regular(regex) => {
+            Pallet::Regular(_) => {
                 let result = self.reg(&mut vec);
                 if result.is_none() {
                     return Ok(vec);
                 }
                 return Err(result.unwrap());
             }
-            Pallet::Name(name) => match self.searching {
+            Pallet::Name(_) => match self.searching {
                 Searching::Absolute => {
                     let result = self.absolute(&mut vec);
                     if result.is_none() {
@@ -74,25 +81,36 @@ impl<'a> Search<'_> {
             Pallet::Regular(re) => {
                 for entry in WalkDir::new(&self.path).same_file_system(true) {
                     let entry = entry.unwrap();
-                    let f_path = entry.path().to_str().unwrap().to_string();
-                    let f_name = entry.file_name().to_str().unwrap().to_string();
-                    Search::reg_search(vec, f_name, re, f_path);
+                    let f_path = entry.path().to_str();
+                    let f_name = entry.file_name().to_str();
+                    if let None = f_path {
+                        return Some(Error::LenPathZero);
+                    }
+                    if let None = f_name {
+                        return Some(Error::LenNameZero);
+                    }
+                    Search::reg_search(
+                        vec,
+                        f_name.unwrap().to_string(),
+                        re,
+                        f_path.unwrap().to_string(),
+                    );
                 }
             }
-            Pallet::Name(name) => {
+            Pallet::Name(_name) => {
                 unreachable!();
             }
         }
         None
     }
-    fn reg_search(vec: &mut Vec<String>, f_name: String, re: &Regex, path: String){
-        for line in re.find_iter(&f_name) {
+    fn reg_search(vec: &mut Vec<String>, f_name: String, re: &Regex, path: String) {
+        for _ in re.find_iter(&f_name) {
             vec.push(path.clone());
         }
     }
     fn absolute(&self, vec: &mut Vec<String>) -> Option<Error> {
         match &self.pallet {
-            Pallet::Regular(regex) => {}
+            Pallet::Regular(_) => {}
             Pallet::Name(name) => {
                 for entry in WalkDir::new(&self.path).same_file_system(true) {
                     let entry = entry.unwrap();
@@ -108,7 +126,7 @@ impl<'a> Search<'_> {
     }
     fn relative(&self, vec: &mut Vec<String>) -> Option<Error> {
         match &self.pallet {
-            Pallet::Regular(regex) => {}
+            Pallet::Regular(_) => {}
             Pallet::Name(name) => {
                 for entry in WalkDir::new(&self.path).same_file_system(true) {
                     let entry = entry.unwrap();
